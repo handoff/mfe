74bc16ed6fa18bc46ea2c5f08783b925dc5bdbc7
Henrik <henrik.andoff@gmail.com>
2023-10-24 13:12:27 +0200

    Add thumb

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>I am the container!</h1>
+      <h1>I am the container! 👍</h1>
       <hr />
       <MarketingApp />
     </div>
